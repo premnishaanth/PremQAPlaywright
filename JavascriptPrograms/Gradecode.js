acfe686ae9168f6ec1b150c03ccc7a5c5883a698
premnishaanth <premnishaanth@users.noreply.github.com>
2026-02-22 14:00:45 +0530

    NewCodeExecrises

---
+++ JavascriptPrograms/Gradecode.js
@@ -0,0 +1,24 @@
+let score = 85; 
+
+if(score>=90 && score <=100){
+    console.log("Your Grade is A");
+}
+else if(score>=80 && score <=89)
+{
+    console.log("Your Grade is B");
+}
+else if(score>=80 && score <=89)
+{
+     console.log("Your Grade is C");
+}
+else if(score>=60 && score <=69)
+{
+     console.log("Your Grade is D");
+}
+else  if(score>=0 && score <=59)
+{
+     console.log("Your Grade is F");
+}
+else{
+        console.log("Invalid Score");
+}
