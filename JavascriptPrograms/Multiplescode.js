fb463e7ea65bb532ed89d7892c7fc1ee7f104054
premnishaanth <premnishaanth@users.noreply.github.com>
2026-02-22 14:05:26 +0530

    Updated Changes

--- JavascriptPrograms/Multiplescode.js
+++ JavascriptPrograms/Multiplescode.js
@@ -1,13 +1,16 @@
 
 for(let Value=1; Value<=100; Value++){
-    if(Value%3==0){
+    if(Value%3 ==0){
         console.log("Fizz");
 }
-else if(Value%5==0){
+else if(Value%5 ==0){
     console.log("Buzz");
 }
-else (Value%3==0 && Value%5==0)
+else if (Value%3==0 && Value%5==0)
 {
     console.log("FizzBuzz");
+}
+else{
+    console.log(Value);         
 }
 }
